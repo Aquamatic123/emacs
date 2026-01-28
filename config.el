700d10ab11e71965a6c03b21606f5ad772d0ee03
Leo Rouleau <leorouleau5070@gmail.com>
2026-01-28 09:11:38 -0500

    fix typo. fix merge conflicts.

--- config.el
+++ config.el
@@ -147,6 +147,7 @@
 (use-package kanagawa-themes
 :ensure t
 :config
+;; 1. wave 2. dragon (dark) 3. lotus (light)
 (load-theme 'kanagawa-wave t))
 
 
@@ -186,6 +187,39 @@
       (kbd "D") 'dired-do-delete       ; Delete
       (kbd "+") 'dired-create-directory))) ; Create new folder
 
+<<<<<<< HEAD
+=======
+;; Frame title
+(setq frame-title-format '("%b - Emacs@" user-login-name))
+
+;; Uncomment this line to remove window decorations
+;;(add-to-list 'default-frame-alist '(undecorated . t))
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode 1)
+
+(use-package which-key
+  :init
+    (which-key-mode 1)
+  :config
+  (setq which-key-side-window-location 'bottom
+	  which-key-sort-order #'which-key-key-order-alpha
+	  which-key-sort-uppercase-first nil
+	  which-key-add-column-padding 1
+	  which-key-max-display-columns nil
+	  which-key-min-display-lines 6
+	  which-key-side-window-slot -10
+	  which-key-side-window-max-height 0.25
+	  which-key-idle-delay 0.8
+	  which-key-max-description-length 25
+	  which-key-allow-imprecise-window-fit t
+	  which-key-separator " → " ))
+
+>>>>>>> 4be824f (fix typo.)
 (use-package vterm
   :ensure t
   :custom
